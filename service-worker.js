8f832b3f332328bf64304a64a25b640a7bf8337c
Neil <neil.roy63@gmail.com>
2021-07-03 13:30:04 +1000

    feature/added service-worker js file

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Used to create an offline cache in the browser & give us access to it from JS
+var cacheName = "hello-pwa";
+
+// List of files that need to be cached
+var filesToCache = ["/", "index.html", "/css/style.css", "/js/main.js"];
+
+// Function to start the service worker and caching all of the app's content
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Function to serve cached content when offline
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
